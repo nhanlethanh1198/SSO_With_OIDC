359c020271815e9ba8154c9d3f3eee4575f22777
Le Thanh Nhan <nhanlethanh1198@gmail.com>
2022-03-19 10:08:57 +0700

    setup

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+
+origins = [
+    'http://localhost:3000',
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/')
+async def index():
+    return {
+        "message": "This is a project for Single Sign On using OpenID Connect"
+    }
--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/')
-async def index():
-    return {
-        "message": "This is a project for Single Sign On using OpenID Connect"
-    }
